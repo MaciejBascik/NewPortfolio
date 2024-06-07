4f4af6fa6d77f3e6dba461a04edebe768f8e8242
MaciejBascik <96304157+MaciejBascik@users.noreply.github.com>
2024-06-07 23:03:47 +0200

    added new section - featured projects and finished Experience section

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ function Home() {
 
 <main className="bg-[#11071F] font-customFont">
 
-  <div className="hero min-h-[70vh]" id="hero" style={{backgroundImage: "url('https://i.postimg.cc/W3mfS3xn/Frame-1.png')"}}>
+  <div className="hero min-h-[90vh]" id="hero" style={{backgroundImage: "url('https://i.postimg.cc/W3mfS3xn/Frame-1.png')"}}>
     <div className="text-[19px] text-left tracking-[2%]  absolute bottom-[75%] ml-[80px]">
       <p>Hello! I Am <span className="text-[#7127BA]">Maciej Baścik</span></p>
       <img className="absolute right-[95%] bottom-[-250%] " src="https://i.postimg.cc/3N5DBXPy/Arrow.png"/> 
@@ -42,44 +42,44 @@ function Home() {
     </div>
     <div className="justify-center items-center flex flex-row ">
       <div className="flex flex-col m-4 gap-4">
-      <div className="w-[80vh] h-[22vh] rounded-b-2xl rounded-t-[5px]" style={{ background: "linear-gradient(110.491deg, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)" }}>
+      <div className="w-[80vh] h-[28vh] rounded-b-2xl rounded-t-[5px]" style={{ background: "linear-gradient(110.491deg, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)" }}>
           <div className="bg-[#4F228D] w-[100%] h-[5px] rounded-t-2xl"></div>
           <div className="flex flex-row items-start">
             <img className="p-[5%] pl-[8%] w-[300px] h-[auto]" src="https://i.ibb.co/5hz8T1F/zaroa.png" />
             <div className="p-[72px] pt-9 pl-5">
-              <h1 className="text-[39px] text-left tracking-[2%] font-[500] font-[Poppins]">Courses</h1>
-              <p className="text-[11px] text-left tracking-[2%] font-[400] font-[Poppins]">I have completed various courses to expand my skills and knowledge, covering both technical and soft skills. Notable courses include "Python from beginner to advanced", "NextJS crash course", and "React crash course", each of which contributes significantly to my professional development.</p>
+              <h1 className="text-[30px] text-left tracking-[2%] font-[500] font-[Poppins]">Courses</h1>
+              <p className="text-[11px] text-left tracking-[2%] font-[400] font-[Poppins]">I have completed courses in "Python from Beginner to Advanced," "NextJS Crash Course," "React Crash Course," and earned the INF 03 qualification, significantly enhancing my skills and professional development.</p>
             </div>
           </div>
         </div>
-        <div className="w-[80vh] h-[22vh] rounded-b-2xl rounded-t-[5px]" style={{ background: "linear-gradient(110.491deg, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)" }}>
+        <div className="w-[80vh] h-[28vh] rounded-b-2xl rounded-t-[5px]" style={{ background: "linear-gradient(110.491deg, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)" }}>
           <div className="bg-[#4F228D] w-[100%] h-[5px] rounded-t-2xl"></div>
           <div className="flex flex-row items-start">
             <img className="p-[5%] pl-[8%] w-[300px] h-[auto]" src="https://i.ibb.co/zQqGfZk/majk.png" />
             <div className="p-[72px] pt-9 pl-5">
-              <h1 className="text-[39px] text-left tracking-[2%] font-[500] font-[Poppins]">Freelancing</h1>
-              <p className="text-[11px] text-left tracking-[2%] font-[400] font-[Poppins]">I have improved my skills by taking on a diverse range of freelancing projects. My freelancing portfolio includes graphic design, web development and mixing engineering. My commitment to delivering high-quality work on time has helped me build a reputation for reliability and excellence in the freelance community.</p>
+              <h1 className="text-[30px] text-left tracking-[2%] font-[500] font-[Poppins]">Freelancing</h1>
+              <p className="text-[11px] text-left tracking-[2%] font-[400] font-[Poppins]">I have honed my skills through various freelancing projects in graphic design, web development, and mixing engineering. My dedication to delivering high-quality work on time has earned me a reputation for reliability and excellence.</p>
             </div>
           </div>
         </div>
     </div>
     <div className="flex flex-col m-4 gap-4">
-        <div className="w-[80vh] h-[22vh] rounded-b-2xl rounded-t-[5px]" style={{ background: "linear-gradient(110.491deg, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)" }}>
+        <div className="w-[80vh] h-[28vh] rounded-b-2xl rounded-t-[5px]" style={{ background: "linear-gradient(110.491deg, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)" }}>
           <div className="bg-[#4F228D] w-[100%] h-[5px] rounded-t-2xl"></div>
           <div className="flex flex-row items-start">
             <img className="p-[5%] pl-[8%] w-[300px] h-[auto]" src="https://i.ibb.co/n864LLB/foto.png" />
             <div className="p-[72px] pt-9 pl-5">
-              <h1 className="text-[39px] text-left tracking-[2%] font-[500] font-[Poppins]">School		</h1>
-              <p className="text-[11px] text-left tracking-[2%] font-[400] font-[Poppins]">My Education journey has been marked by high achievements, including the highest grades at computer science subjects. Through projects and collaborations, I've cultivated critical thinking and problem-solving skills essential for professional success.</p>
+              <h1 className="text-[30px] text-left tracking-[2%] font-[500] font-[Poppins]">School		</h1>
+              <p className="text-[11px] text-left tracking-[2%] font-[400] font-[Poppins]">My education includes top grades in computer science and has developed my critical thinking and problem-solving skills through various projects and collaborations.</p>
             </div>
           </div>
         </div>
-        <div className="w-[80vh] h-[22vh] rounded-b-2xl rounded-t-[5px]" style={{ background: "linear-gradient(110.491deg, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)" }}>
+        <div className="w-[80vh] h-[28vh] rounded-b-2xl rounded-t-[5px]" style={{ background: "linear-gradient(110.491deg, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)" }}>
           <div className="bg-[#4F228D] w-[100%] h-[5px] rounded-t-2xl"></div>
           <div className="flex flex-row items-start">
             <img className="p-[5%] pl-[8%] w-[300px] h-[auto]" src="https://i.ibb.co/TWbMQRV/kubek.png" />
             <div className="p-[72px] pt-9 pl-5">
-              <h1 className="text-[39px] text-left tracking-[2%] font-[500] font-[Poppins]">Work</h1>
+              <h1 className="text-[30px] text-left tracking-[2%] font-[500] font-[Poppins]">Work</h1>
               <p className="text-[11px] text-left tracking-[2%] font-[400] font-[Poppins]">My career includes a position at PapillonMedia, where I excelled in project management and fast task execution. Clients have distinguished me for my professionalism.</p>
             </div>
           </div>
@@ -91,22 +91,29 @@ function Home() {
     
 
 
-
-  <div className="hero min-h-screen" id="signup"
-    style={{backgroundImage: 'url(https://i.ibb.co/gmbsT6L/Sign-up.png)', objectFit:"cover"}}>
-    <div className="text-center text-neutral-content items-center" >
-        <button className="btn btn-sm bg-transparent h-9 mb-2 text-[14px] hover:bg-transparent"
-          style={{color:"black"}}>Make your life easier </button>
-        <h1
-          className="mb-5  lg:text-[80px] md:text-[50px] sx:text-[45px] text-[47px] mx-[20px] font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#000000] to-[#001354]">
-          Join our community</h1>
-        <p
-          className="mb-5 md:mx-[180px] mx-[20px] lg:text-[22px]  md:text-[17px] sm:text-[18px] text-[18px]  text-transparent bg-clip-text bg-gradient-to-r from-[#000000] to-[#001354]">
-          Are you always on the lookout for the latest fashion trends? Join our community by signing up for our app and stay ahead of the curve! Our app is your ultimate destination for finding trending clothes effortlessly. Here&apos;s why you should sign up now:</p>
-          
-    </div>
     
+    <div className="hero min-h-[100vh] font-customFont" id="hero">
+    <div className="flex  flex-col items-center justify-center ">
+    <p className="text-[24px] text-center tracking-[2%] font-[400] max-w-[100%]">I'm currently looking to join a <span className="text-[#7127BA]">cross-functional</span> team <br></br>
+    <span className="text-[17px] ">that values improving people's lives through accessible design</span> </p>
+        <img className="w-[80%] mt-8" src="https://i.ibb.co/Y0FM4Fx/Skills.png"/>
+    </div>
   </div>
+
+
+
+  <div className="hero min-h-[100vh] font-[Poppins] " id="hero">
+  
+  <div className="flex  flex-col max-w-[50%]">
+    <p className="text-[16px] text-left tracking-[2%] max-w-[100%] text-[#9857D3] font-bold">Featured  Project</p>
+    <p className="text-[34px] text-left tracking-[2%] max-w-[100%] text-[#CCD6F6]">ViralClothes</p>
+    <div className="rounded-2xl  p-10 backdrop-blur-sm" >
+      <p className="text-[18px] text-left tracking-[2%] max-w-[100%] text-[#CCD6F6]">ViralClothes is an innovative application designed to help users stay on top of the latest fashion trends on TikTok. By leveraging keyword-based search algorithms, ViralClothes curates a dynamic list of trending clothing items, ensuring users are always in vogue.</p>
+    </div>
+  </div>
+
+</div>
+
 
 </main>
 
